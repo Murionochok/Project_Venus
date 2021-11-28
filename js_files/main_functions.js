ef2b7dece03783088bfada8b68d2aba9422e2cf3
whole-galaxy <dimon.skorohodov@gmail.com>
2021-11-28 17:11:56 +0200

    Спроба створити слайдер, чомусь не відображається

--- js_files/main_functions.js
+++ js_files/main_functions.js
@@ -11,4 +11,38 @@ const cardsList = new CardsList({
 
 cardBlock.append(cardsList.element);
 
+document.addEventListener('DOMContentLoaded', function () {
+    // инициализация слайдера
+    new SimpleAdaptiveSlider('.slider', {
+      loop: true,
+      autoplay: true,
+      interval: 5000,
+      swipe: true,
+    });
+  });
 
+
+  document.addEventListener('DOMContentLoaded', function () {
+    // инициализация слайдера
+    var slider = new SimpleAdaptiveSlider('.slider');
+    // назначим обработчик при нажатии на кнопку .btn-prev
+    document.querySelector('.btn-prev').onclick = function () {
+      // перейдём к предыдущему слайду
+      slider.prev();
+    }
+    // назначим обработчик при нажатии на кнопку .btn-next
+    document.querySelector('.btn-next').onclick = function () {
+      // перейдём к следующему слайду
+      slider.next();
+    }
+  });
+
+
+
+
+
+
+
+
+
+ 
