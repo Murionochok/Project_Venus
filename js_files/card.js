dc6871955e49efe98df5d37657766401ae3731b3
Volos Ivan <ivan.volos1@gmail.com>
2021-12-02 20:36:27 +0200

    Minor changes #2

--- js_files/card.js
+++ js_files/card.js
@@ -2,18 +2,18 @@ export default class Card {
     element;
 
     constructor({
-        id = '',
-        title = '',
-        description = '',
-        location = '',
-        price = 0,
-        startDate = new Date(),
-        endDate = new Date(),
-        hasTentPlace = false,
-        imageLink = '',
-        LinkToOffSite = ''
-    } = {}) {
-        this.id= id;
+                    id = '',
+                    title = '',
+                    description = '',
+                    location = '',
+                    price = 0,
+                    startDate = new Date(),
+                    endDate = new Date(),
+                    hasTentPlace = false,
+                    imageLink = '',
+                    LinkToOffSite = ''
+                } = {}) {
+        this.id = id;
         this.title = title;
         this.description = description;
         this.location = location;
@@ -25,33 +25,6 @@ export default class Card {
         this.LinkToOffSite = LinkToOffSite;
 
         this.render();
-    }
-
-getHTMLTemplate(){
-    return `
-        <div class="festivals__window">
-            ${this.title}
-            <div class="festivals-descr">
-                <div class="festivals__window-box">
-                    <div class="festivals__window-descr">${this.description}</div>
-                <button class="festivals__window-button">Details</button>
-                </div>
-            </div>
-        </div>
-    `
-}
-
-    render() {
-        const wrapper = document.createElement('div');
-        wrapper.innerHTML = this.getHTMLTemplate();
-        this.element = wrapper.firstElementChild;
-    }
-
-    destroy() {
-        if (this.element) {
-            this.element.remove();
-        }
-        this.element = null;
     }
 }
 
--- js_files/main_functions.js
+++ js_files/main_functions.js
@@ -1,7 +1,6 @@
-import Card from './card.js';
-import CardsList from "./card-list.js";
 import { festivals } from './festivals.js';
 
+console.log('it works');
 document.addEventListener('DOMContentLoaded', function () {
     // инициализация слайдера
     const slider = new SimpleAdaptiveSlider('.slider', {
@@ -22,14 +21,60 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 });
 
-let cardBlock = document.getElementById('root');
+let festivalBlock = document.getElementsByClassName('big-festival-container');
 
-const cardsList = new CardsList({
-    data: festivals,
-    Component: Card
-});
+function onStart() {
 
-cardBlock.append(cardsList.element);
+    let this_month = [2, 3, 5, 12];
+    let popular = [1, 3, 5, 7];
+    let ukraine = [1, 7, 3, 4, 5, 6];
+
+    let value_for_this_month = '';
+    let value_for_popular = '';
+    let value_for_ukraine = '';
+    let value_for_main = '';
+
+    festivals.forEach((festival) => {
+        let htmlBlock = `
+        <div id="fest${festival.id}" class="festivals__window">
+          <div class="festivals-descr">
+            <div class="festivals__window-box-column">
+              <div class="festivals__window-date">${festival.startDate}</div>
+              <div class="festivals__window-title">${festival.title}</div>
+              <div class="festivals__window-descr">${festival.description}</div>         <!--Приклад вікна-->
+              <div class="festivals__window-location">${festival.location}</div>
+              <div class="festivals__window-price"><b>${festival.price}</b></div>
+              <a class="festivals__window-button" href="${festival.LinkToOffSite}">Купити квиток</a>
+            </div>
+          </div>
+        </div>
+        `;
+        let smallHtmlBlock = `
+        <div id="fest${festival.id}" class="small-festivals__window">
+          <div class="card-header"></div>
+          <div class="descr">${festival.title}</div>
+        </div>`;
+        if(this_month.includes(JSON.parse(festival.id))){
+            value_for_this_month += htmlBlock;
+        }
+        if(popular.includes(JSON.parse(festival.id))){
+            value_for_popular += htmlBlock;
+        }
+        if(ukraine.includes(JSON.parse(festival.id))){
+            value_for_ukraine += smallHtmlBlock;
+        }
+        value_for_main += htmlBlock;
+    });
+
+
+    festivalBlock[0].innerHTML = value_for_this_month;
+    festivalBlock[1].innerHTML = value_for_popular;
+    festivalBlock[2].innerHTML = value_for_main;
+    festivalBlock[3].innerHTML = value_for_ukraine;
+}
+
+onStart();
+
 
 
 
